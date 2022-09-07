4a9aa2f9e2fe6066a014603babd5c5a6dd32cbb2
Andrea Prina <and.prina@gmail.com>
2022-09-07 17:24:43 +0200

    Create header and footer blade templates

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,37 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-4 pt-4 pb-4">
+                <div class="container-fluid p-0">
+                    <div class="row">
+                        <div class="col-4">
+                            <h5>DC COMICS</h5>
+                            <ul class="list-unstyled">
+                                <li>LINK</li>
+                            </ul>
+                            <h5>SHOP</h5>
+                            <ul class="list-unstyled">
+                                <li>LINK</li>
+                            </ul>
+                        </div>
+                        <div class="col-4">
+                            <h5>DC</h5>
+                            <ul class="list-unstyled">
+                                <li>LINK</li>
+                            </ul>
+                        </div>
+                        <div class="col-4">
+                            <h5>SITES</h5>
+                            <ul class="list-unstyled">
+                                <li>LINK</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-8 position-relative overflow-hidden">
+                <img class="background-logo" src="" alt="">
+            </div>
+        </div>
+    </div>
+</footer>
--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -4,11 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
     
     @include("includes.header")
+
+    <main>
+
+    </main>
+
+    @include("includes.footer")
 
 </body>
 </html>
